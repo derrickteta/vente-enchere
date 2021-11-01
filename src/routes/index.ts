6f83dba2b27a3e274d8fa3389451869622be50af
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-01 16:48:18 +0100

    feat: add routes in App.tsx and routes file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,14 @@
 export const ROUTES = {
   HOME_PAGE: '/',
+  SIGNIN: '/signin',
+  SIGNUP: '/signup',
+  ADMIN_PAGE: {
+    DASHBOARD: '/admin/dashboard',
+  },
+  VENDEUR_PAGE: {
+    DASHBOARD: '/vendeur/dashboard',
+  },
+  GERANT_PAGE: {
+    DASHBOARD: '/gerant/dashboard',
+  },
 };
