f0d52221241f7b7c77267ec6357d1f11bdf764a3
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-02 22:58:36 +0100

    feat: update styles in Carousel and Search bar components

--- src/modules/homePage/components/SearchBar/index.tsx
+++ src/modules/homePage/components/SearchBar/index.tsx
@@ -9,6 +9,7 @@ const SearchContainer = styled.div`
   padding: 15px;
   background-color: white;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  margin-bottom: 50px;
 
   > div {
     display: flex;
