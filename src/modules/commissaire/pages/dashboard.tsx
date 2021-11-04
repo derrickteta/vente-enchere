655d00488dc1c066fa70ebf7253f278f369706c5
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-04 07:42:44 +0100

    feat: create commissaire module

---
+++ src/modules/commissaire/pages/dashboard.tsx
@@ -0,0 +1,9 @@
+import { CommissaireContainer } from '../components/CommissaireContainer';
+
+export const CommissaireDashboard = () => {
+  return (
+    <CommissaireContainer clicked='dashboard'>
+      <h2>Commissaire Dashboard</h2>
+    </CommissaireContainer>
+  );
+};
