e571f6f73215665cc5b5dc0bfbfd9b875f3b59e3
Talom <franklinfrost14@gmail.com>
2021-11-04 14:09:11 +0100

    feat: update props of Layout component

--- src/modules/shared/Layout/index.tsx
+++ src/modules/shared/Layout/index.tsx
@@ -4,17 +4,17 @@ import { Header } from './header';
 export const Layout = ({
   children,
   other,
+  maxWidth = 1000,
 }: {
   children: ReactNode;
   other?: ReactNode;
+  maxWidth?: number;
 }) => {
   return (
     <div>
       <Header />
       <>{other}</>
-      <div style={{ maxWidth: 1000, margin: 'auto', padding: 20 }}>
-        {children}
-      </div>
+      <div style={{ maxWidth, margin: 'auto', padding: 20 }}>{children}</div>
     </div>
   );
 };
