ce9f9fb9b3e2094749e8f90c54d0ee161135e193
Cesar MH <nohocesar@gmail.com>
2021-11-10 08:21:59 +0100

    feat: create gerant products page

---
+++ src/modules/gerant/pages/products.tsx
@@ -0,0 +1,11 @@
+import { GerantContainer } from '../components/GerantContainer';
+
+export const GerantProducts = () => {
+  return (
+    <GerantContainer clicked='produits'>
+      <div>
+        <h2>Gerant Mes Produits</h2>
+      </div>
+    </GerantContainer>
+  );
+};
--- src/modules/gerant/routes/gerant.routes.tsx
+++ src/modules/gerant/routes/gerant.routes.tsx
@@ -1,10 +1,12 @@
 import { Route } from 'react-router-dom';
 import { GerantDashboard } from '../pages/dashboard';
+import { GerantProducts } from '../pages/products';
 
 export const GerantRoutes = () => {
   return (
     <>
       <Route exact path='/gerant/dashboard' component={GerantDashboard} />
+      <Route exact path='/gerant/products' component={GerantProducts} />
     </>
   );
 };
