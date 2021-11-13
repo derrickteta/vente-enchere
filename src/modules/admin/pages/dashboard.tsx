e1d61abf92685e08416d9e361f2559042c598ca0
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-13 06:16:12 +0100

    feat: update Table design and use in admin dashboard

--- src/modules/admin/pages/dashboard.tsx
+++ src/modules/admin/pages/dashboard.tsx
@@ -1,3 +1,4 @@
+import { Button, Space, Tag, Tooltip } from 'antd';
 import {
   FaChartLine,
   FaDollarSign,
@@ -8,7 +9,11 @@ import {
   FaUserAlt,
   FaUsersCog,
 } from 'react-icons/fa';
+import { FiTrash2 } from 'react-icons/fi';
+import { ButtonWithModal } from '../../shared/ButtonWithModal';
 import { StatsCard } from '../../shared/StatsCard';
+import { DataTable } from '../../shared/Table';
+import { dateFormatter } from '../../shared/Table/cellFormatter';
 import { AdminContainer } from '../components/AdminContainer';
 
 export const AdminDashBoard = () => {
@@ -25,6 +30,9 @@ export const AdminDashBoard = () => {
           />
         ))}
       </div>
+
+      <h2 style={{ marginTop: 50 }}>Utilisateurs</h2>
+      <DataTable loading={false} data={[]} columns={userColumns} />
     </AdminContainer>
   );
 };
@@ -77,5 +85,110 @@ const Stats = [
     text: 'Compte bancaire',
     stat: 111850,
     gradientColors: ['#ff7e5f', '#feb47b'],
+  },
+];
+
+const userColumns = [
+  {
+    title: 'Nom & Prénom',
+    key: 'nom',
+    render: (cell: any, row: any) => (
+      <span>
+        {row.nom} {row.prenom}{' '}
+      </span>
+    ),
+  },
+  {
+    title: 'Adresse',
+    dataIndex: 'localisation',
+    key: 'adresse',
+    render: (cell: any, row: any) => <span>{cell.adresse} </span>,
+  },
+  {
+    title: 'Téléphone',
+    dataIndex: 'telephone',
+    key: 'telephone',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'compte',
+    key: 'email',
+    render: (cell: any, row: any) => <span>{cell.email} </span>,
+  },
+  {
+    title: 'Roles',
+    dataIndex: 'roles',
+    key: 'roles',
+    render: (cell: string[], row: any) => (
+      <Space>
+        {cell.map((item) => (
+          <Tag key={item} color='magenta'>
+            {item}
+          </Tag>
+        ))}
+      </Space>
+    ),
+  },
+  {
+    title: 'Total dépensé',
+    dataIndex: 'totalArgentDepense',
+    key: 'totalArgentDepense',
+    render: (cell: any, row: any) => <span>{cell} FCFA </span>,
+  },
+  {
+    title: 'Etat du compte',
+    dataIndex: 'compte',
+    key: 'isActivated',
+    render: (cell: any, row: any) => <span>{cell.isActivated} </span>,
+  },
+  {
+    title: "Date d'inscription",
+    dataIndex: 'dateAjout',
+    key: 'dateAjout',
+    render: dateFormatter,
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (cell: any, row: any) => (
+      <Tooltip title="supprimer l'article">
+        <ButtonWithModal
+          buttonText={<FiTrash2 />}
+          buttonProps={{ danger: true }}
+          modalProps={{ title: 'Confirmation' }}
+        >
+          {(closeModal) => (
+            <div>
+              <h3>Voulez vous supprimer cet utilisateur ?</h3>
+              <Space>
+                <Button type='primary' onClick={() => closeModal()}>
+                  Fermer
+                </Button>
+                <Button
+                  danger
+                  onClick={async () => {
+                    // if (response.success) {
+                    //   notification.success({
+                    //     message: 'Succès',
+                    //     description: response.message,
+                    //   });
+                    //   window.location.reload();
+                    // } else {
+                    //   notification.success({
+                    //     message: 'Erreur',
+                    //     description: response.message,
+                    //   });
+                    // }
+                    closeModal();
+                  }}
+                >
+                  Oui
+                </Button>
+              </Space>
+            </div>
+          )}
+        </ButtonWithModal>
+      </Tooltip>
+    ),
   },
 ];
