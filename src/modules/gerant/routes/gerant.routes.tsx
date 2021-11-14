0b6d1f8e2fffa3a13b9b5310030e8d669fad5d02
Cesar MH <nohocesar@gmail.com>
2021-11-14 18:17:33 +0100

    feat: update gerant routes

--- src/modules/gerant/routes/gerant.routes.tsx
+++ src/modules/gerant/routes/gerant.routes.tsx
@@ -1,13 +1,15 @@
 import { Route } from 'react-router-dom';
 import { GerantAccount } from '../pages/account';
 import { GerantDashboard } from '../pages/dashboard';
-import { GerantProducts } from '../pages/products';
+import { GerantVendorDetail } from '../pages/details';
+import { GerantVendors } from '../pages/vendors';
 
 export const GerantRoutes = () => {
   return (
     <>
       <Route exact path='/gerant/dashboard' component={GerantDashboard} />
-      <Route exact path='/gerant/products' component={GerantProducts} />
+      <Route exact path='/gerant/vendors' component={GerantVendors} />
+      <Route exact path='/gerant/details' component={GerantVendorDetail} />
       <Route exact path='/gerant/my-account' component={GerantAccount} />
     </>
   );
