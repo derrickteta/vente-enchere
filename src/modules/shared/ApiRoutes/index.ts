30dce46aa81fa6b8ed8e14eb51de3615c850a6a4
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-15 13:32:26 +0100

    feat: update apiroutes fil

--- src/modules/shared/ApiRoutes/index.ts
+++ src/modules/shared/ApiRoutes/index.ts
@@ -1,4 +1,5 @@
 export const BASE_URL = 'http://192.168.43.215:3001/';
+// export const BASE_URL = 'https://vente-enchere-api.herokuapp.com/';
 
 export const API_ROUTES = {
   SIGNUP: (role: string) => `${BASE_URL}api/auth/signup/${role}`,
