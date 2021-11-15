0cba5eccfa48f931bdc6476b9c822ca83821af5f
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-15 14:11:15 +0100

    feat: add lot and produit routes in API_ROUTES

--- src/modules/shared/ApiRoutes/index.ts
+++ src/modules/shared/ApiRoutes/index.ts
@@ -4,4 +4,12 @@ export const BASE_URL = 'http://192.168.43.215:3001/';
 export const API_ROUTES = {
   SIGNUP: (role: string) => `${BASE_URL}api/auth/signup/${role}`,
   SIGNIN: `${BASE_URL}api/auth/signin`,
+  PRODUITS: {
+    BASE: `${BASE_URL}api/produits`,
+    BASEID: (id: string) => `${BASE_URL}api/produits/${id}`,
+  },
+  LOTS: {
+    BASE: `${BASE_URL}api/lots`,
+    BASEID: (id: string) => `${BASE_URL}api/lots/${id}`,
+  },
 };
