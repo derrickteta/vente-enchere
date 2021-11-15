651f60e72021f9035d95406e980f8fd17b537776
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-15 13:48:16 +0100

    feat: add put and delete in customFetch

--- src/shared/customFetch.ts
+++ src/shared/customFetch.ts
@@ -10,4 +10,19 @@ export const customFetch = {
       body: JSON.stringify(data),
     }).then((res) => res.json());
   },
+
+  put: (url: string, data: unknown) => {
+    return fetch(url, {
+      method: 'put',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    }).then((res) => res.json());
+  },
+
+  delete: (url: string) => {
+    return fetch(url, {
+      method: 'delete',
+      headers: { 'Content-Type': 'application/json' },
+    }).then((res) => res.json());
+  },
 };
