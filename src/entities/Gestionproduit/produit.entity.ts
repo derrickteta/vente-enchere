800ffc87803f4df58da8dc8add7dce4c49d55b1e
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-16 04:03:12 +0100

    fix: correct name error of 'description' in produit entity

--- src/entities/Gestionproduit/produit.entity.ts
+++ src/entities/Gestionproduit/produit.entity.ts
@@ -1,13 +1,18 @@
 import { VendeurEntity } from '../GestionCompte/vendeur.entity';
 import { CategorieEntity } from './categorie.entity';
 
+export type QuantityType = {
+  valeur: number;
+  unite: string;
+};
 export class ProduitEntity {
   _id: string;
   nom: string;
-  descirption: string;
-  prixMin: string;
+  description: string;
+  prixMin: number;
+  quantite: QuantityType;
   vendeur: VendeurEntity;
-  category: CategorieEntity;
+  categorie: CategorieEntity;
   images: string[];
   estBio: boolean;
   statut: string;
@@ -18,10 +23,11 @@ export class ProduitEntity {
   constructor(produit: ProduitEntity) {
     this._id = produit._id;
     this.nom = produit.nom;
-    this.descirption = produit.descirption;
+    this.description = produit.description;
     this.prixMin = produit.prixMin;
+    this.quantite = produit.quantite;
     this.vendeur = produit.vendeur;
-    this.category = produit.category;
+    this.categorie = produit.categorie;
     this.images = produit.images;
     this.estBio = produit.estBio;
     this.statut = produit.statut;
