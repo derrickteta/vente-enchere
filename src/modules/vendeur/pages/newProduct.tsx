ef19b096c2610f9826d240578e3020edcf2527c6
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-16 08:27:31 +0100

    feat: update NewProduct page with steps

--- src/modules/vendeur/pages/newProduct.tsx
+++ src/modules/vendeur/pages/newProduct.tsx
@@ -1,12 +1,94 @@
+import { Button, Divider, Input, Modal, Space, Steps } from 'antd';
+import { useState } from 'react';
+import { FaRegSmileWink } from 'react-icons/fa';
+import { PRIMARY } from '../../../shared/colors';
 import { ProductForm } from '../components/ProductForm';
 import { VendeurContainer } from '../components/VendeurContainer';
 
 export const NewProductPage = () => {
+  const [numProduit, setNumProduit] = useState(1);
+  const [currentNum, setCurrentNum] = useState(0);
+  const [isModalVisible, setIsModalVisible] = useState(true);
+
   return (
     <div>
       <VendeurContainer clicked='products'>
-        <h1>Creation d'un produit</h1>
-        <ProductForm />
+        <Modal
+          visible={isModalVisible}
+          title='Informations'
+          onCancel={() => setIsModalVisible(!isModalVisible)}
+          footer={null}
+          maskClosable={false}
+        >
+          <h2 style={{ textAlign: 'center' }}>
+            Combien de produit souhaitez vous avoir dans votre lot ?
+          </h2>
+          <Space
+            direction='vertical'
+            style={{ alignItems: 'center', width: '100%' }}
+          >
+            <Input
+              placeholder='nombre de produit'
+              onChange={(event) => setNumProduit(Number(event.target.value))}
+            />
+            <Button type='primary' onClick={() => setIsModalVisible(false)}>
+              Continuer
+            </Button>
+          </Space>
+        </Modal>
+
+        {!isModalVisible && (
+          <>
+            <Steps current={currentNum} style={{ marginBottom: 30 }}>
+              {[...Array(numProduit)].map((item, index) => (
+                <Steps.Step title={<span>Produit N° {index + 1} </span>} />
+              ))}
+              <Steps.Step
+                status='wait'
+                title='Terminé'
+                icon={<FaRegSmileWink />}
+              />
+            </Steps>
+            {currentNum === numProduit ? (
+              <div
+                style={{
+                  height: '50vh',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <h2 style={{ textAlign: 'center' }}>
+                  Nous vous remercions. Vous produits ont tous été créé avec
+                  succès. Ils seronts prochainement validés par le commissaire
+                  priseur de Agric Auction
+                </h2>
+                <Button
+                  type='primary'
+                  size='large'
+                  style={{
+                    backgroundColor: PRIMARY,
+                    borderWidth: 0,
+                  }}
+                >
+                  Terminer
+                </Button>
+              </div>
+            ) : (
+              <>
+                <h2>Creation du produit N° {currentNum + 1} </h2>
+                <div style={{ maxWidth: '20%', marginBottom: 50 }}>
+                  <Divider style={{ backgroundColor: '#777', height: 2 }} />
+                </div>
+                <ProductForm
+                  currentNum={currentNum}
+                  setCurrentNum={setCurrentNum}
+                />
+              </>
+            )}
+          </>
+        )}
       </VendeurContainer>
     </div>
   );
