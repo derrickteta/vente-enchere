cb89d0de1d6abfe6bd1618865151972e03fa7d41
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-16 05:16:28 +0100

    feat: add fetchCategories, addImageProduit adn update createProduit in vendeur network

--- src/modules/vendeur/network/index.tsx
+++ src/modules/vendeur/network/index.tsx
@@ -1,9 +1,35 @@
+import { CategorieEntity } from '../../../entities/Gestionproduit/categorie.entity';
+import { LotEntity } from '../../../entities/Gestionproduit/lot.entity';
+import { ProduitEntity } from '../../../entities/Gestionproduit/produit.entity';
+import { ResponseType } from '../../../entities/Response.entity';
 import { customFetch } from '../../../shared/customFetch';
 import { API_ROUTES } from '../../shared/ApiRoutes';
 
-export const createProduit = (produit: any) => {
+export const createProduit = (
+  produit: any,
+): Promise<ResponseType<ProduitEntity>> => {
   return customFetch.post(API_ROUTES.PRODUITS.BASE, produit);
 };
-export const createLot = (lot: any) => {
+
+export const addImageProduit = (
+  produitId: string,
+  images: [],
+): Promise<ResponseType<ProduitEntity>> => {
+  let formData = new FormData();
+  for (let image of images) {
+    formData.append('image', image);
+  }
+
+  return fetch(API_ROUTES.PRODUITS.ADD_IMAGES(produitId), {
+    method: 'put',
+    body: formData,
+  }).then((res) => res.json());
+};
+
+export const createLot = (lot: any): Promise<ResponseType<LotEntity>> => {
   return customFetch.post(API_ROUTES.LOTS.BASE, lot);
+};
+
+export const fetchCategories = (): Promise<ResponseType<CategorieEntity>> => {
+  return customFetch.get(API_ROUTES.CATEGORIES.BASE);
 };
