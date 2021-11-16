247e812fbde41b043eddd18c1b544d951100e4fc
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-16 08:39:49 +0100

    feat: add images route in API_ROUTES to get images

--- src/modules/shared/ApiRoutes/index.ts
+++ src/modules/shared/ApiRoutes/index.ts
@@ -4,6 +4,7 @@ export const BASE_URL = 'http://192.168.43.215:3001/';
 export const API_ROUTES = {
   SIGNUP: (role: string) => `${BASE_URL}api/auth/signup/${role}`,
   SIGNIN: `${BASE_URL}api/auth/signin`,
+  IMAGES: (id: string) => `${BASE_URL}api/images/${id}`,
   PRODUITS: {
     BASE: `${BASE_URL}api/produits`,
     BASEID: (id: string) => `${BASE_URL}api/produits/${id}`,
