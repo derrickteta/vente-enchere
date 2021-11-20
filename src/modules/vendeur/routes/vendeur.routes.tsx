0447784ab7ac08e59812243bd61f78474443f3c7
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-20 07:45:23 +0100

    feat: protect routes in vendeur routes

--- src/modules/vendeur/routes/vendeur.routes.tsx
+++ src/modules/vendeur/routes/vendeur.routes.tsx
@@ -1,4 +1,5 @@
 import { Route } from 'react-router-dom';
+import { ProtectedRoutes } from '../../shared/ProtectedRoutes';
 import { VendeurAccountPage } from '../pages/account';
 import { VendeurDashboard } from '../pages/dashboard';
 import { NewProductPage } from '../pages/newProduct';
@@ -6,11 +7,11 @@ import { VendeurProductPage } from '../pages/product';
 
 export const VendeurRoutes = () => {
   return (
-    <>
+    <ProtectedRoutes authorization='vendeur'>
       <Route exact path='/vendeur/dashboard' component={VendeurDashboard} />
       <Route exact path='/vendeur/products' component={VendeurProductPage} />
       <Route exact path='/vendeur/products/new' component={NewProductPage} />
       <Route exact path='/vendeur/account' component={VendeurAccountPage} />
-    </>
+    </ProtectedRoutes>
   );
 };
