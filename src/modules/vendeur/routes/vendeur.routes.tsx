5cfb14c810be711ff7d08454d8fa9be2ce639d3a
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-21 23:43:16 +0100

    feat: create ImageCarousel component

--- src/modules/vendeur/routes/vendeur.routes.tsx
+++ src/modules/vendeur/routes/vendeur.routes.tsx
@@ -1,5 +1,4 @@
 import { Route } from 'react-router-dom';
-import { ProtectedRoutes } from '../../shared/ProtectedRoutes';
 import { VendeurAccountPage } from '../pages/account';
 import { VendeurDashboard } from '../pages/dashboard';
 import { NewProductPage } from '../pages/newProduct';
@@ -7,11 +6,11 @@ import { VendeurProductPage } from '../pages/product';
 
 export const VendeurRoutes = () => {
   return (
-    <ProtectedRoutes authorization='vendeur'>
+    <>
       <Route exact path='/vendeur/dashboard' component={VendeurDashboard} />
       <Route exact path='/vendeur/products' component={VendeurProductPage} />
       <Route exact path='/vendeur/products/new' component={NewProductPage} />
       <Route exact path='/vendeur/account' component={VendeurAccountPage} />
-    </ProtectedRoutes>
+    </>
   );
 };
