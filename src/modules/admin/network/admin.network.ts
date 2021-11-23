7915d73dfd0980442596740a9313d6aefa1c312f
Talom_Franklin <franklinfrost14@gmail.com>
2021-11-23 02:58:00 +0100

    feat: add fetch users in admin network file

--- src/modules/admin/network/admin.network.ts
+++ src/modules/admin/network/admin.network.ts
@@ -1,3 +1,8 @@
-export const TestFunction = () => {
-  // delete this function
+import { UserEntity } from '../../../entities/GestionCompte/user.entity';
+import { ResponseType } from '../../../entities/Response.entity';
+import { customFetch } from '../../../shared/customFetch';
+import { API_ROUTES } from '../../shared/ApiRoutes';
+
+export const fetchUsers = (): Promise<ResponseType<UserEntity>> => {
+  return customFetch.get(API_ROUTES.USERS.BASE);
 };
