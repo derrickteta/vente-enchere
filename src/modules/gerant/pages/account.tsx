97556436d1dcff336e9b5155730a9e157272ec79
Cesar MH <nohocesar@gmail.com>
2021-11-24 15:12:14 +0100

    feat: update header of gerant account page

--- src/modules/gerant/pages/account.tsx
+++ src/modules/gerant/pages/account.tsx
@@ -1,20 +1,8 @@
-import { Button } from 'antd';
-import ButtonGroup from 'antd/lib/button/button-group';
 import { GerantContainer } from '../components/GerantContainer';
-
-const headerChildren: JSX.Element = (
-  <>
-    <h2>Text</h2>
-    <ButtonGroup>
-      <Button>Ajouter</Button>
-      <Button>Supprimer</Button>
-    </ButtonGroup>
-  </>
-);
 
 export const GerantAccount = () => {
   return (
-    <GerantContainer clicked='account' headerChildren={headerChildren}>
+    <GerantContainer clicked='account'>
       <div>
         <h2>Gerant compte</h2>
       </div>
