bf33b932f4409d756b8e09295e35ad09e27a0934
Cesar MH <nohocesar@gmail.com>
2021-12-07 22:34:36 +0100

    fix: Enchere Entity Definition

--- src/entities/GestionEnchere/enchere.entity.ts
+++ src/entities/GestionEnchere/enchere.entity.ts
@@ -2,14 +2,14 @@ import { LotEntity } from '../Gestionproduit/lot.entity';
 
 export type StatusType = 'en_cours' | 'termine';
 
-export class salleEnchereEntity {
+export class EnchereEntity {
   _id: string;
   lots: LotEntity[];
   dateOuverture: string;
   duree: number;
   status: StatusType;
 
-  constructor(enchere: salleEnchereEntity) {
+  constructor(enchere: EnchereEntity) {
     this._id = enchere._id;
     this.lots = enchere.lots;
     this.dateOuverture = enchere.dateOuverture;
