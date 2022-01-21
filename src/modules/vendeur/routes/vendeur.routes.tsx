b71f4d043245408f82a6fd524220e1863764dcac
Voldemort12107 <61709849+Voldemort12107@users.noreply.github.com>
2022-01-21 13:19:54 +0100

    Modification de route dans vendeur pour l'événement

--- src/modules/vendeur/routes/vendeur.routes.tsx
+++ src/modules/vendeur/routes/vendeur.routes.tsx
@@ -2,14 +2,14 @@ import { Route } from 'react-router-dom';
 import { VendeurAccountPage } from '../pages/account';
 import { VendeurDashboard } from '../pages/dashboard';
 import { DetailLot } from '../pages/detailLot';
+import { VendeurEventPage } from '../pages/evenement';
 import { NewProductPage } from '../pages/newProduct';
-import { VendeurProductPage } from '../pages/product';
 
 export const VendeurRoutes = () => {
   return (
     <>
       <Route exact path='/vendeur/dashboard' component={VendeurDashboard} />
-      <Route exact path='/vendeur/products' component={VendeurProductPage} />
+      <Route exact path='/vendeur/event' component={VendeurEventPage} />
       <Route exact path='/vendeur/products/new' component={NewProductPage} />
       <Route exact path='/vendeur/account' component={VendeurAccountPage} />
       <Route path='/vendeur/lot-details' component={DetailLot} />
