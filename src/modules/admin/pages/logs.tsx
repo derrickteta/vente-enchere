d61010fed8a6d7db503da3893dcef7ec8ac37da5
Voldemort12107 <61709849+Voldemort12107@users.noreply.github.com>
2022-01-21 15:46:37 +0100

    Modification de la page d'affichage des logs pour l'admin

--- src/modules/admin/pages/logs.tsx
+++ src/modules/admin/pages/logs.tsx
@@ -1,11 +1,53 @@
+import { useEffect, useState } from 'react';
+import { LogEntity } from '../../../entities/GestionLog/log.entity';
+import { DataTable } from '../../shared/Table';
+import { dateFormatter } from '../../shared/Table/cellFormatter';
 import { AdminContainer } from '../components/AdminContainer';
+import { fecthLogs } from '../network/admin.network';
 
 export const LogPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [log, setLog] = useState<LogEntity[]>([]);
+
+  useEffect(() => {
+    fecthLogs().then((data) => {
+      if (data.success) {
+        setLog(data.result);
+        setIsLoading(false);
+      }
+    });
+  }, []);
+
   return (
     <AdminContainer clicked='log'>
       <div>
-        <h2>Logs page</h2>
+        <h2 style={{ marginTop: 50 }}>Logs page</h2>
+        <DataTable loading={isLoading} data={log} columns={colms} />
       </div>
     </AdminContainer>
   );
 };
+/*date_creation, action, createur, table*/
+const colms = [
+  {
+    title: 'Date Creation',
+    dataIndex: 'date_creation',
+    key: 'date_creation',
+    render: dateFormatter,
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    dataIndex: 'action',
+  },
+  {
+    title: 'Createur',
+    key: 'createur',
+    dataIndex: 'createur',
+  },
+  {
+    title: 'Table',
+    key: 'table',
+    dataIndex: 'table',
+  },
+];
